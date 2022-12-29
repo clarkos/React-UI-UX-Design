6f194a550a20da65eda32b39eb294b2640270d0b
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2022-12-29 23:32:58 +0000

    Initialize Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { Article, Brand, CTA, Feature, Navbar } from './components'
+
+const App = () => {
+  return (
+    <div><h1>App</h1> </div>
+  )
+}
+
+export default App
