8054a33147264a9ce72131317f8d35dd21b44bda
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2023-01-01 22:34:03 +0000

    a half way on

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+          porro at et molestiae nihil sunt molestias natus rerum fugit, corrupti
+          eos earum, iste blanditiis quos libero illo officiis totam ea.
+          consectetur atque quibusdam incidunt.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Enter your e-mail" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} />
+          <p>1600 people request access a visit in last 24hs</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ad voluptates quia optio explicabo fugiat nisi deleniti natus asperiores voluptate, repellat, blanditiis maiores ratione ducimus. Pariatur corrupti debitis aperiam maxime?"
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The posibilities are beyond your Imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbot"
+          text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ad voluptates quia optio explicabo fugiat nisi deleniti natus asperiores voluptate, repellat, blanditiis maiores ratione ducimus. Pariatur corrupti debitis aperiam maxime?"
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ad voluptates quia optio explicabo fugiat nisi deleniti natus asperiores voluptate, repellat, blanditiis maiores ratione ducimus. Pariatur corrupti debitis aperiam maxime?"
+        />
+        <Feature
+          title="Education"
+          text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. At ad voluptates quia optio explicabo fugiat nisi deleniti natus asperiores voluptate, repellat, blanditiis maiores ratione ducimus. Pariatur corrupti debitis aperiam maxime?"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
